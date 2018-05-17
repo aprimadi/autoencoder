6beb406aed1121297209027c991f37aa200f5980
Armin Primadi <aprimadi@gmail.com>
2018-05-17 11:59:55 +0700

    Adding deep autoencoder

---
+++ deep_autoencoder.py
@@ -0,0 +1,66 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+from keras import regularizers
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_img = Input(shape=(784,))
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
+
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
+
+# This model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# This model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+
+encoded_input = Input(shape=(32,))
+decoded_layer = autoencoder.layers[-3](encoded_input)
+decoded_layer = autoencoder.layers[-2](decoded_layer)
+decoded_layer = autoencoder.layers[-1](decoded_layer)
+decoder = Model(encoded_input, decoded_layer)
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+autoencoder.fit(x_train, x_train,
+                epochs=100,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+print(encoded_imgs.mean())
