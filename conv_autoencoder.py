5644fced4326dc181a16a96bae42a77fa41e887a
Armin Primadi <aprimadi@gmail.com>
2018-05-18 10:24:30 +0700

    Annotate convolutional autoencoder

--- conv_autoencoder.py
+++ conv_autoencoder.py
@@ -8,22 +8,22 @@ import matplotlib.pyplot as plt
 
 input_img = Input(shape=(28, 28, 1))
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)    # ???
-x = MaxPooling2D((2, 2), padding='same')(x)                             # ???
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)    # (28, 28, 16)
+x = MaxPooling2D((2, 2), padding='same')(x)                             # (14, 14, 16)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)             # (14, 14, 8)
+x = MaxPooling2D((2, 2), padding='same')(x)                             # (7, 7, 8)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)             # (7, 7, 8)
+encoded = MaxPooling2D((2, 2), padding='same')(x)                       # (4, 4, 8)
 
 # At this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)       # (4, 4, 8)
+x = UpSampling2D((2, 2))(x)                                             # (8, 8, 8)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)            # (8, 8, 8)
+x = UpSampling2D((2, 2))(x)                                             # (16, 16, 8)
+x = Conv2D(16, (3, 3), activation='relu')(x)                            # (14, 14, 8)
+x = UpSampling2D((2, 2))(x)                                             # (28, 28, 8)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)    # (28, 28, 1)
 
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
@@ -33,16 +33,11 @@ encoder = Model(input_img, encoded)
 (x_train, _), (x_test, _) = mnist.load_data()
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-print(x_train.shape)
-print(x_test.shape)
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
-print(x_train.shape)
-print(x_test.shape)
 
 autoencoder.fit(x_train, x_train,
-                # epochs=50,
-                epochs=20,
+                epochs=50,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test),
